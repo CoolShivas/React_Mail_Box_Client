813b698d3e3c5609ed3c0dde24b764439c940447
CoolShivas <shivas2710cool00@gmail.com>
2024-06-05 02:04:12 +0530

    Stitching done with event and target value to get the values on console.

--- mail_box_client/src/auth/SignUp.jsx
+++ mail_box_client/src/auth/SignUp.jsx
@@ -1,8 +1,21 @@
 import styles from "./SignUp.module.css";
 
 const SignUp = () => {
+
+    const handlerOnSubmitForm = (e) => {
+        e.preventDefault();
+
+        const signUpData = {
+            Email: e.target.email.value,
+            Password: e.target.pass.value,
+            ConfirmPassword: e.target.confirmpass.value,
+        };
+
+        console.log(signUpData);
+    };
+
     return (
-        <div className={styles.signup_div}>
+        <form className={styles.signup_div} onSubmit={handlerOnSubmitForm}>
             <div className={styles.heading}>
                 <h1> Sign Up </h1>
             </div>
@@ -12,7 +25,7 @@ const SignUp = () => {
                 </label>
 
                 <div className="col-sm-10">
-                    <input type="email" className="form-control" />
+                    <input type="email" className="form-control" id="email" required />
                 </div>
             </div>
 
@@ -21,24 +34,23 @@ const SignUp = () => {
                     Password
                 </label>
                 <div className="col-sm-10">
-                    <input type="password" className="form-control" />
+                    <input type="password" className="form-control" id="pass" minLength={6} required />
                 </div>
             </div>
 
             <div className="mb-3 row">
                 <label htmlFor="inputPassword" className="col-sm-2 col-htmlForm-label">
-                    {" "}
                     Confirm Password
                 </label>
                 <div className="col-sm-10">
-                    <input type="password" className="form-control" />
+                    <input type="password" className="form-control" id="confirmpass" minLength={6} required />
                 </div>
             </div>
 
             <div className={styles.actions_signup__btn}>
-                <button> Submit </button>
+                <button type="submit"> Submit </button>
             </div>
-        </div>
+        </form>
     );
 };
 
