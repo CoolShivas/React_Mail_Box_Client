bbc8a2e0bb8e49bcb2b4575d134f8e9962d83f15
CoolShivas <shivas2710cool00@gmail.com>
2024-06-08 17:45:48 +0530

    After Refreshing the page Header remains with buttons login and logout functionality with token.

--- mail_box_client/src/components/Header.jsx
+++ mail_box_client/src/components/Header.jsx
@@ -1,9 +1,28 @@
+import { authActions } from "../store";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import styles from "./Header.module.css";
 import Navbar from "./Navbar";
 
 
 
 const Header = () => {
+
+    const dispatch = useDispatch();
+
+    const isAuthenticated = useSelector((store) => store.authen.isAuthenticated);
+    console.log(isAuthenticated);
+
+
+    let localToken = localStorage.getItem("Save Token");
+
+    useEffect(() => {
+
+        if (localToken) {
+            dispatch(authActions.login(localToken));
+        }
+
+    }, [localToken]);
 
     return (
         <>
