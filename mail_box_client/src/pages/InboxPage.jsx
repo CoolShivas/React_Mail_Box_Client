c7929c675fe0687d9089da9fe1a3b659231a4897
CoolShivas <shivas2710cool00@gmail.com>
2024-06-11 10:40:13 +0530

    Getting the data from server's route inbox with the help of useEffect hook.

--- mail_box_client/src/pages/InboxPage.jsx
+++ mail_box_client/src/pages/InboxPage.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from "react";
 import styles from "./InboxPage.module.css";
 
 const InboxPage = () => {
+
+    const cleanEmail = localStorage.getItem("cleanEmail");
+    console.log(cleanEmail);
+    // Getting the cleanEmail i.e, stored in localStorage ;
+
+    const [sentMails, setSentMails] = useState([]);
+    // Going to store the get mails from inbox server by api get call to this sentMails Array;
+
+    useEffect(() => {
+        const fetchInboxEmailFromServer = async () => {
+
+            try {
+
+                const response = await fetch(
+                    `https://mailboxclient-88eb9-default-rtdb.firebaseio.com/electronicMaiL/${cleanEmail}/inbox.json`
+                );
+
+                if (!response.ok) {
+                    throw new Error("Unable to fetch inbox email from server");
+                }
+
+                const data = await response.json();
+                console.log(data); // Getting the data;
+
+
+            } catch (error) {
+                console.log("Something went wrong inbox emails", error);
+            }
+
+        };
+        fetchInboxEmailFromServer();
+    }, [cleanEmail]);
 
     return (<>
 
